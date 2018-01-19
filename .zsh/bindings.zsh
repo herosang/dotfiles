0e905db9dc9189a255d02d7cc40cb6f027e8d54d
Hairuo Sang <hairuo.sang@gmail.com>
2018-01-18 23:28:14 -0800

    Fixed ur bindings
    was looking a little weak

--- .zsh/bindings.zsh
+++ .zsh/bindings.zsh
@@ -2,7 +2,7 @@
 bindkey -v
 
 # <ctr-k>/<ctrl-j> to navigate up/down line history
-bindkey '^k' up-line-or-history
+bindkey '^p' up-line-or-history
 bindkey '^j' down-line-or-history
 # <ctrl-g> to move to bottom of history
 bindkey '^g' end-of-buffer-or-history
